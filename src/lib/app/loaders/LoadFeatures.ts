2e2f41d5a7dec2edce9107303bfe8c8587e3c5de
necipsunbul <ns@necipsunbul.com>
2024-05-16 02:54:28 +0300

    fix: swagger endpoint run development mode

--- src/lib/app/loaders/LoadFeatures.ts
+++ src/lib/app/loaders/LoadFeatures.ts
@@ -24,9 +24,11 @@ export default class LoadFeatures extends BaseApplicationLoader{
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use("/static", express.static("public"));
         this.app.use(cors(corsOptions));
-        if (process.env.NODE_ENV === ApplicationMode.dev) this.app.use(morgan("dev"));
         this.app.use(FileUpload(FileUploadConfig));
-        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
+        if (process.env.NODE_ENV === ApplicationMode.dev) {
+            this.app.use(morgan("dev"));
+            this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
+        }
         this.app.use(routes);
     }
 }
