c775b30d5d993b87fa8542fd8cdc18cfc3068c8a
necipsunbul <ns@necipsunbul.com>
2024-05-17 02:23:17 +0300

    added projection param on findAll and findOne functions & convertObjectId function modified with throw exception <- BaseMongoDbService.ts

---
+++ src/lib/core/contants/ErrorMessageContants.ts
@@ -0,0 +1,6 @@
+export default class ErrorMessageConstants {
+    static badRequest = 'Bad Request';
+
+    //exceptions
+    static invalidObjectIdFormat = 'Invalid object id format';
+}
--- src/lib/core/services/MongoDbService.ts
+++ src/lib/core/services/MongoDbService.ts
@@ -1,6 +1,7 @@
-import {Model, Types, UpdateQuery} from "mongoose";
+import {FilterQuery, Model, ProjectionType, Types, UpdateQuery} from "mongoose";
 import BaseEntityModel from "../base/BaseEntityModel";
 import BaseDbService from "../base/BaseDbService";
+import ErrorMessageConstants from "../contants/ErrorMessageContants";
 
 export default class BaseMongoService<T extends Model<any>, M extends BaseEntityModel> extends BaseDbService {
     constructor(private readonly model: T) {
@@ -10,34 +11,34 @@ export default class BaseMongoService<T extends Model<any>, M extends BaseEntity
         const willSaveData = new this.model(data);
         return willSaveData.save();
     }
-    findAll(condition = {}) {
-        return this.model.find(condition);
+    findAll(condition :FilterQuery<any> = {}, projection : ProjectionType<any> = {}) {
+        return this.model.find(condition, projection);
     }
-    findOne(condition = {}) {
-        return this.model.findOne(condition);
+    findOne(condition :FilterQuery<any> = {},projection : ProjectionType<any> = {}) {
+        return this.model.findOne(condition,projection);
     }
-    async deleteOne(condition = {}) {
+    async deleteOne(condition : FilterQuery<any> = {}) {
         const result = await this.model.deleteOne(condition);
         return result.deletedCount;
     }
-    deleteMany(condition = {}) {
+    deleteMany(condition:FilterQuery<any> = {}) {
         return this.model.deleteMany(condition);
     }
-    async updateOne(condition = {}, data: UpdateQuery<M>) {
+    async updateOne(condition:FilterQuery<any> = {}, data: UpdateQuery<M>) {
         const result = await this.model.updateOne(condition, data);
         return result.modifiedCount;
     }
-    findOneAndUpdate(condition = {}, data: UpdateQuery<M>) {
+    findOneAndUpdate(condition :FilterQuery<any> = {}, data: UpdateQuery<M>) {
         return this.model.findOneAndUpdate(condition, data, { upsert: true });
     }
-    updateMany(condition = {}, data: UpdateQuery<M>) {
+    updateMany(condition:FilterQuery<any> = {}, data: UpdateQuery<M>) {
         return this.model.updateMany(condition, data);
     }
     protected  isObjectId(id:any) : boolean{
         return Types.ObjectId.isValid(id);
     }
     protected convertObjectId(id:string) : Types.ObjectId | false {
-        if(!this.isObjectId(id)) return false;
+        if(!this.isObjectId(id)) throw new Error(ErrorMessageConstants.invalidObjectIdFormat);
         return new Types.ObjectId(id);
     }
 }
