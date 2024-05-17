682a0545d98abcfdf302b801291d94ddd208af53
necipsunbul <ns@necipsunbul.com>
2024-05-17 18:48:10 +0300

    fix: AppError class body

--- src/lib/core/error/AppError.ts
+++ src/lib/core/error/AppError.ts
@@ -1,9 +1,14 @@
 export default class AppError extends Error {
     public httpStatus?: number;
     public errorCode?: number;
-    constructor(message: string, httpStatus?: number, errorCode?: number) {
+    constructor(message: string, codes?:IAppErrorCodes) {
         super(message);
-        this.httpStatus = httpStatus;
-        this.errorCode = errorCode;
+        this.httpStatus = codes?.httpStatus;
+        this.errorCode = codes?.errorCode;
     }
+}
+
+export interface IAppErrorCodes{
+    httpStatus?: number;
+    errorCode?: number;
 }
