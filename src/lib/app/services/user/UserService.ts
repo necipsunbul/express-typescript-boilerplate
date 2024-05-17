3f3a12c1c9744d5bcbec8e46fd710c96a83feca0
necipsunbul <ns@necipsunbul.com>
2024-05-17 17:54:07 +0300

    fix: example user service class entended base service

--- src/lib/app/services/user/UserService.ts
+++ src/lib/app/services/user/UserService.ts
@@ -1,4 +1,8 @@
-class UserService{
+import BaseService from "../../../core/base/BaseService";
+import UserRepository from "./UserRepository";
+
+class UserService extends BaseService{
+    repo : UserRepository = new UserRepository();
     getUser(param:string){
         // logic
     }
