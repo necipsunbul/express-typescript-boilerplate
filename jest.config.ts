c82af5b02977e3ba12085ad8bb784344c1941931
necipsunbul <ns@necipsunbul.com>
2024-06-13 23:09:29 +0300

    fix: forceExit parameter added  ->  jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     clearMocks: true,
+    forceExit: true,
     coverageProvider: "v8",
     preset: "ts-jest",
     testEnvironment: "node",
