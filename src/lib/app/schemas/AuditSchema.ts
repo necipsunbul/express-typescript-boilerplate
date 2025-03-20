2645ce5876d50d2753e3ce5559075c355b524db1
necipsunbul <ns@necipsunbul.com>
2025-03-20 15:34:30 +0300

    cursor rule files cretead and module create rules added

--- src/lib/app/schemas/AuditSchema.ts
+++ src/lib/app/schemas/AuditSchema.ts
@@ -1,19 +1,19 @@
-import { Schema } from "mongoose";
+import { Schema } from 'mongoose';
 
 const auditSchema = new Schema(
-    {
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-        createdBy: Schema.Types.ObjectId,
-        updatedAt: Date,
-        updatedBy: Schema.Types.ObjectId,
+  {
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
-    {
-        versionKey: false,
-        _id: false,
-    }
+    createdBy: Schema.Types.ObjectId,
+    updatedAt: Date,
+    updatedBy: Schema.Types.ObjectId,
+  },
+  {
+    versionKey: false,
+    _id: false,
+  }
 );
 
 export default auditSchema;
--- src/lib/app/services/user/UserMongoService.ts
+++ src/lib/app/services/user/UserMongoService.ts
@@ -1,10 +1,10 @@
-import { Model } from "mongoose";
-import BaseMongoService from "../../../core/services/MongoDbService";
-import {UserEntity} from "./UserEntity";
-import UserSchema , {IUser} from "../../schemas/UserSchema";
+import { Model } from 'mongoose';
+import BaseMongoService from '../../../core/services/MongoDbService';
+import { UserEntity } from './UserEntity';
+import UserSchema, { IUser } from './UserSchema';
 
-export default class UserMongoService extends BaseMongoService<Model<IUser>, UserEntity>{
-    constructor() {
-        super(UserSchema);
-    }
+export default class UserMongoService extends BaseMongoService<Model<IUser>, UserEntity> {
+  constructor() {
+    super(UserSchema);
+  }
 }
