e722f3acd59ef7a321a98a043a3d39d3879e0032
Necip Sünbül <ns@necipsunbul.com>
2025-07-24 22:02:01 +0300

    fix FileMnager getRootPath value

--- src/lib/core/local_files/FileManager.ts
+++ src/lib/core/local_files/FileManager.ts
@@ -1,53 +1,53 @@
-import path from "path";
-import fs from "fs";
+import path from 'path';
+import fs from 'fs';
 class FileManager {
-    private static _instance?: FileManager;
-    private constructor() { }
-    public static get instance() :FileManager {
-        if(!this._instance) this._instance = new FileManager();
-        return this._instance;
-    }
-    getRootPath(): string {
-        return process.env.PWD + "/dist";
-    }
+  private static _instance?: FileManager;
+  private constructor() {}
+  public static get instance(): FileManager {
+    if (!this._instance) this._instance = new FileManager();
+    return this._instance;
+  }
+  getRootPath(): string {
+    return process.env.PWD as string;
+  }
 
-    PWD() : string {
-        return process.env.PWD as string;
-    }
+  PWD(): string {
+    return process.env.PWD as string;
+  }
 
-    getPath(folderList: string[] = []) :string {
-        return path.join(this.getRootPath(), ...folderList);
-    }
+  getPath(folderList: string[] = []): string {
+    return path.join(this.getRootPath(), ...folderList);
+  }
 
-     createFile(fileName: string, content: Buffer) {
-        return new Promise((resolve, reject) => {
-            fs.writeFile(fileName, content, (err) => {
-                if (err) reject(err);
-                resolve(true);
-            });
-        });
-    }
+  createFile(fileName: string, content: Buffer) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(fileName, content, (err) => {
+        if (err) reject(err);
+        resolve(true);
+      });
+    });
+  }
 
-     readFile(filePath: string) {
-        return new Promise((resolve, reject) => {
-            fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
-                if (!err) return resolve(data);
-                reject(err);
-            });
-        });
-    }
+  readFile(filePath: string) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+        if (!err) return resolve(data);
+        reject(err);
+      });
+    });
+  }
 
-    joinPath(folders: string[]) {
-        return path.join(...folders);
-    }
+  joinPath(folders: string[]) {
+    return path.join(...folders);
+  }
 
-    removeFileFromServer(filePath: string) {
-        if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
-    }
+  removeFileFromServer(filePath: string) {
+    if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+  }
 
-    getFileExtension(fileName: string) {
-        return path.extname(fileName).replace(".", "");
-    }
+  getFileExtension(fileName: string) {
+    return path.extname(fileName).replace('.', '');
+  }
 }
 
 export default FileManager;
